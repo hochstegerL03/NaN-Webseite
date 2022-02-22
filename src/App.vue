45c3f287228a4d3377429707dbac5a6df2faa020
Daniel Diewald <diewald.d02@htlwienwest.at>
2022-02-22 09:03:07 +0100

    v.0.0.1
    setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container d-flex flex-column align-items-center">
+    <h2>
+      <span class="text-danger"><i class="far fa-heart"></i></span>
+      {{ msg }}
+      <span class="text-danger"><i class="far fa-heart"></i></span>
+    </h2>
+    <img src="@/assets/logo.svg" alt="Vue logo" height="100" />
+    <SayHello name="Robert"></SayHello>
+  </div>
+</template>
+
+<script setup>
+import SayHello from '@/components/SayHello.vue';
+
+const msg = 'Willkommen!';
+</script>
+
+<style>
+</style>
